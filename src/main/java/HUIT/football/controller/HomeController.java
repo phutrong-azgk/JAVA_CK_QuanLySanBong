1c2650e3894453a2eab9ae0549bf2d306ef1d110
phutrong-azgk <linhtrongtt@gmail.com>
2026-06-14 13:22:03 +0700

    vào hồ sơ đầu

--- src/main/java/HUIT/football/controller/HomeController.java
+++ src/main/java/HUIT/football/controller/HomeController.java
@@ -2,12 +2,16 @@ package HUIT.football.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Authentication authentication) {
+        if (authentication != null && authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_KHACH"))) {
+            return "redirect:/profile";
+        }
         return "redirect:/san";
     }
 }
